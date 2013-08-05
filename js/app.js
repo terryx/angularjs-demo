fa37d480244ebb059ec853b4d45374420d67c559
Terry Yuen <terryxlife@gmail.com>
2013-08-05 13:20:43 +0800

    Delete functionality

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var app = angular.module('App', []);
 
-app.controller('Todo', ['$scope', function($scope){
+app.controller('Todo', ['$scope', function ($scope) {
 
     $scope.user = {}; //form
     var count = 1;
 
     $scope.rows = [];
 
-    $scope.save = function(user){
+    $scope.save = function (user) {
 
         user.id = count;
         $scope.rows.push(user);
@@ -16,5 +16,14 @@ app.controller('Todo', ['$scope', function($scope){
 
         //clear form data
         $scope.user = {};
+    }
+
+    $scope.remove = function (user) {
+        for (var i = 0, len = $scope.rows.length; i < len; i++) {
+            if ($scope.rows[i].id === user.id) {
+                $scope.rows.splice(i, 1);
+                break;
+            }
+        }
     }
 }]);
